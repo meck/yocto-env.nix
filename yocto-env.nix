acee04cd802a1f79e57b093bf27ee78c3b0edd25
meck <Johan@meck.se>
2025-11-07 15:13:11 +0100

    Use bash

--- yocto-env.nix
+++ yocto-env.nix
@@ -7,61 +7,7 @@
 let
   inherit (pkgsForInteractiveShell) lib;
 
-  histFile = "~/.history-yocto-env";
-
-  zshConfig = pkgsForInteractiveShell.writeTextFile {
-    name = "zshrc";
-    text = ''
-      export HISTFILE="$(realpath ${histFile})"
-      export GRML_COMP_CACHING="0"
-
-      source ${pkgsForInteractiveShell.grml-zsh-config}/etc/zsh/zshrc
-
-      # Prompt modifications.
-      #
-      # In current grml zshrc, changing `$PROMPT` no longer works,
-      # and `zstyle` is used instead, see:
-      # https://unix.stackexchange.com/questions/656152/why-does-setting-prompt-have-no-effect-in-grmls-zshrc
-
-      # Disable the grml `sad-smiley` on the right for exit codes != 0;
-      # it makes copy-pasting out terminal output difficult.
-      # Done by setting the `items` of the right-side setup to the empty list
-      # (as of writing, the default is `items sad-smiley`).
-      # See: https://bts.grml.org/grml/issue2267
-      zstyle ':prompt:grml:right:setup' items
-
-      # Add nix-shell indicator that makes clear when we're in nix-shell.
-      # Described in: http://bewatermyfriend.org/p/2013/003/
-      nix_shell_prompt() {
-          REPLY=''${IN_NIX_SHELL+"(yocto-env) "}
-      }
-      grml_theme_add_token nix-shell-indicator -f nix_shell_prompt '%F{magenta}' '%f'
-
-      zstyle ':prompt:grml:left:setup' items rc change-root user at host path vcs \
-                                       nix-shell-indicator percent
-
-
-
-      alias l='${pkgsForInteractiveShell.eza}/bin/eza -l'
-      alias ls='${pkgsForInteractiveShell.eza}/bin/eza'
-
-      export LESSOPEN="|${pkgsForInteractiveShell.lesspipe}/bin/lesspipe.sh %s"
-      source ${pkgsForInteractiveShell.fzf}/share/fzf/completion.zsh
-      source ${pkgsForInteractiveShell.fzf}/share/fzf/key-bindings.zsh
-    '';
-    destination = "/.zshrc";
-  };
-
-  zshBin = pkgsForInteractiveShell.writeShellApplication {
-    name = "zsh";
-    runtimeInputs = [ pkgsForInteractiveShell.zsh ];
-    text = ''
-      export SHELL_SESSIONS_DISABLE=1
-      ZDOTDIR=${zshConfig} ${pkgsForInteractiveShell.zsh}/bin/zsh -o NO_GLOBAL_RCS
-    '';
-  };
-
-  fhs = pkgsForInteractiveShell.buildFHSUserEnvBubblewrap {
+  fhs = pkgsForInteractiveShell.buildFHSEnvBubblewrap {
     name = "yocto-env";
     targetPkgs = pkgs: (with pkgsForYocto; let
       libxcrypt = pkgsForYocto.libxcrypt or null; # Nixpkgs 20.03
@@ -153,7 +99,7 @@ let
         export BB_ENV_EXTRAWHITE="$BB_ENV_PASSTHROUGH_ADDITIONS"
       '';
     runScript = ''
-      ${zshBin}/bin/zsh
+      ${pkgsForInteractiveShell.bash}/bin/bash
     '';
   };
 in
